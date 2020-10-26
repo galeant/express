82ceaf514232bc6f040d5162d4293da84f5e1415
ilham <ilham.fadillah@noxus.co.id>
2020-10-26 12:20:03 +0700

    route test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var kadalRouter = require('./routes/kadal');
 
 var app = express();
 
@@ -13,14 +14,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// ?
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// route file to path
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/kadal', kadalRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
